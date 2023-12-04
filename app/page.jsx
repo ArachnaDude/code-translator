bf53a8c4ebdb083b456b4d820cc2f9d58b406414
Matt <cunning.distraction@googlemail.com>
2023-12-04 01:42:46 +0000

    imported morseLookup

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,10 @@
+import { morseLookup } from "./morseLookup";
+
 export default function Home() {
   return (
     <main>
       <h2>Code Translator</h2>
+      <p>{`${morseLookup.H}${morseLookup.E}${morseLookup.L}${morseLookup.L}${morseLookup.O}`}</p>
     </main>
   );
 }
