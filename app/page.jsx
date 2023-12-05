db82ae45d489fe009cd83fdd3e920cf5c6adae10
Matt <cunning.distraction@googlemail.com>
2023-12-05 00:22:44 +0000

    add functionality for hover and text rules

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,24 @@
+"use client";
+
 import { translate } from "./morseLookup";
+import { useState } from "react";
 
 export default function Home() {
+  const [isHover, setHover] = useState(false);
+
+  const englishText = "Morse Code Translator";
+  const morseText = translate(englishText);
+
   return (
     <main>
-      <h2>Code Translator</h2>
-      <p>{translate("hello world")}</p>
+      <h2
+        className="morse-code"
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+      >
+        {isHover ? morseText : englishText}
+      </h2>
+      <p>Hello World</p>
     </main>
   );
 }
