aa5e32b3bc8ce970b55b0e8e9f80e506662b8860
Matt <cunning.distraction@googlemail.com>
2023-12-05 02:00:44 +0000

    refactored homepage to make use of new util file

--- app/page.jsx
+++ app/page.jsx
@@ -1,24 +1,44 @@
 "use client";
 
 import { translate } from "./morseLookup";
-import { useState } from "react";
+import useHover from "./useHover";
 
 export default function Home() {
-  const [isHover, setHover] = useState(false);
+  const {
+    isHovered: isHoveredH2,
+    handleMouseEnter: handleMouseEnterH2,
+    handleMouseLeave: handleMouseLeaveH2,
+  } = useHover();
+
+  const {
+    isHovered: isHoveredButton,
+    handleMouseEnter: handleMouseEnterButton,
+    handleMouseLeave: handleMouseLeaveButton,
+  } = useHover();
 
   const englishText = "Morse Code Translator";
   const morseText = translate(englishText);
+
+  const buttonText = "submit";
+  const morseButton = translate(buttonText);
 
   return (
     <main>
       <h2
         className="morse-code"
-        onMouseEnter={() => setHover(true)}
-        onMouseLeave={() => setHover(false)}
+        onMouseEnter={handleMouseEnterH2}
+        onMouseLeave={handleMouseLeaveH2}
       >
-        {isHover ? morseText : englishText}
+        {isHoveredH2 ? morseText : englishText}
       </h2>
       <p>Hello World</p>
+
+      <button
+        onMouseEnter={handleMouseEnterButton}
+        onMouseLeave={handleMouseLeaveButton}
+      >
+        {isHoveredButton ? morseButton : buttonText}
+      </button>
     </main>
   );
 }
