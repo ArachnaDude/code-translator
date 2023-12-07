e99f99bc64a60d37b3072f2efcf4889c1416f681
Matt <cunning.distraction@googlemail.com>
2023-12-07 02:24:15 +0000

    add textarea and clear button for handling translation

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@
 
 import { translate } from "./morseLookup";
 import useHover from "./useHover";
+import { useState } from "react";
 
 export default function Home() {
   const {
@@ -11,21 +12,47 @@ export default function Home() {
   } = useHover();
 
   const {
-    isHovered: isHoveredButton,
-    handleMouseEnter: handleMouseEnterButton,
-    handleMouseLeave: handleMouseLeaveButton,
+    isHovered: isHoveredSubmitButton,
+    handleMouseEnter: handleMouseEnterSubmitButton,
+    handleMouseLeave: handleMouseLeaveSubmitButton,
+  } = useHover();
+
+  const {
+    isHovered: isHoveredClearButton,
+    handleMouseEnter: handleMouseEnterClearButton,
+    handleMouseLeave: handleMouseLeaveClearButton,
   } = useHover();
 
   const englishText = "Morse Code Translator";
   const morseText = translate(englishText);
 
-  const buttonText = "submit";
-  const morseButton = translate(buttonText);
+  const buttonSubmitText = "submit";
+  const morseSubmitButton = translate(buttonSubmitText);
+
+  const buttonClearText = "clear";
+  const morseClearButton = translate(buttonClearText);
+
+  const [input, setInput] = useState("");
+
+  const handleChange = (event) => {
+    setInput(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const morseText = translate(input);
+    const textArea = document.getElementById("translator");
+    textArea.value = morseText;
+  };
+
+  const handleClear = (event) => {
+    console.log("cleared");
+    setInput("");
+  };
 
   return (
     <main>
       <h2
-        className="morse-code"
+        className="text-5xl"
         onMouseEnter={handleMouseEnterH2}
         onMouseLeave={handleMouseLeaveH2}
       >
@@ -33,12 +60,29 @@ export default function Home() {
       </h2>
       <p>Translate English into Morse Code</p>
 
-      <button
-        onMouseEnter={handleMouseEnterButton}
-        onMouseLeave={handleMouseLeaveButton}
-      >
-        {isHoveredButton ? morseButton : buttonText}
-      </button>
+      <form onSubmit={handleSubmit} onReset={handleClear}>
+        <label htmlFor="translator"></label>
+        <textarea
+          className="border-4"
+          id="translator"
+          rows="4"
+          cols="50"
+          placeholder="Type your message in here, and see it translated to Morse Code!"
+          value={input}
+          onChange={handleChange}
+        ></textarea>{" "}
+        <br />
+        <button type="submit">
+          {isHoveredSubmitButton ? morseSubmitButton : buttonSubmitText}
+        </button>
+        <button
+          type="reset"
+          onMouseEnter={handleMouseEnterClearButton}
+          onMouseLeave={handleMouseLeaveClearButton}
+        >
+          {isHoveredClearButton ? morseClearButton : buttonClearText}
+        </button>
+      </form>
     </main>
   );
 }
